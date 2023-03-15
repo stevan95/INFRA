c3b17079ebfba75cc89e827163aee3048c24c1cc
Stevan <mstevan95@gmail.com>
2023-03-15 21:54:47 +0100

    IF-9: Implement session

--- code/simple-go/app/cmd/web/main.go
+++ code/simple-go/app/cmd/web/main.go
@@ -7,13 +7,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
 )
+
+var app config.AppConfig
 
 const portNumber = ":8080"
 
+// To make session variable accessable in all main package. Important note is to set session := scs.New()
+// to session = scs.New() to make these two variables same
+var session *scs.SessionManager
+
 // main is the main function
 func main() {
-	var app config.AppConfig
+
+	//Change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
