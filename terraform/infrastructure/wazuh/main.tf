b896f8f006ba16d98f2a02c1d35603176e4d1456
Stevan <mstevan95@gmail.com>
2023-05-27 15:52:46 +0200

    Wazuh dashboard

--- terraform/infrastructure/wazuh/main.tf
+++ terraform/infrastructure/wazuh/main.tf
@@ -2,6 +2,106 @@ provider "aws" {
   region = "us-west-2"
 }
 
+module "ec2_wazuh_dashboard" {
+  source = "../../modules/ec2"
+
+  sg_name     = "Wazuh-SG-Dashboard"
+  description = "Allow SSH from ansible host and allow trafic on port 443"
+  vpc_id      = module.vpc.vpc_id
+
+  security-group-ingress-settings = [
+    {
+      "port"              = "22"
+      "protocol"          = "tcp"
+      "cidr_block"        = ""
+      "sg_id" = module.ec2_ansible.sg-id
+    },
+    {
+      "port"              = "5601"
+      "protocol"          = "tcp"
+      "cidr_block"        = "0.0.0.0/0"
+      "sg_id" = ""
+    }
+  ]
+
+  security-group-egress-settings = [
+    {
+      "port"              = "0"
+      "protocol"          = "-1"
+      "cidr_block"        = "0.0.0.0/0"
+    }
+  ]
+
+  ec2_configuration = {
+    ami           = "ami-0ac64ad8517166fb1"
+    instance_type = "t3.large"
+    subnet_id     = module.vpc.public_subnet_ids[0]
+    associate_public_ip_address = true
+
+  }
+
+  tag_name = "Wazuh-dashboard"
+}
+
+module "ec2_wazuh_server" {
+  source = "../../modules/ec2"
+
+  sg_name     = "Wazuh-Server-SG"
+  description = "Allow SSH from ansible host and allow trafic on following ports"
+  vpc_id      = module.vpc.vpc_id
+
+  security-group-ingress-settings = [
+    {
+      "port"              = "22"
+      "protocol"          = "tcp"
+      "cidr_block"        = ""
+      "sg_id" = module.ec2_ansible.sg-id
+    },
+    {
+      "port"              = "1514"
+      "protocol"          = "tcp"
+      "cidr_block"        = "0.0.0.0/0"
+      "sg_id" = ""
+    },
+    {
+      "port"              = "1515"
+      "protocol"          = "tcp"
+      "cidr_block"        = "0.0.0.0/0"
+      "sg_id" = ""
+    },
+    {
+      "port"              = "55000"
+      "protocol"          = "tcp"
+      "cidr_block"        = "0.0.0.0/0"
+      "sg_id" = ""
+    },
+    {
+      "port"              = "514"
+      "protocol"          = "udp"
+      "cidr_block"        = "0.0.0.0/0"
+      "sg_id" = ""
+    }
+  ]
+
+  security-group-egress-settings = [
+    {
+      "port"              = "0"
+      "protocol"          = "-1"
+      "cidr_block"        = "0.0.0.0/0"
+    }
+  ]
+
+  ec2_configuration = {
+    ami           = "ami-0ac64ad8517166fb1"
+    instance_type = "t3.large"
+    subnet_id     = module.vpc.private_subnet_ids[0]
+    associate_public_ip_address = false
+
+  }
+
+  tag_name = "Wazuh-server"
+}
+
 module "ec2_wazuh_indexer" {
   source = "../../modules/ec2"
 
@@ -40,7 +140,7 @@ module "ec2_wazuh_indexer" {
 
   }
 
-  tag_name = "Wazuh"
+  tag_name = "Wazuh-indexer"
 }
 
 module "ec2_ansible" {
@@ -72,7 +172,6 @@ module "ec2_ansible" {
     instance_type = "t3.micro"
     subnet_id     = module.vpc.public_subnet_ids[0]
     associate_public_ip_address = true
-
   }
 
   tag_name = "Ansible"
