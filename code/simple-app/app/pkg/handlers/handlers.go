2f5a6925498b6abcefb6653258b7923018d35b17
Stevan <mstevan95@gmail.com>
2023-07-04 01:49:44 +0200

    Fix

--- code/simple-app/app/pkg/handlers/handlers.go
+++ code/simple-app/app/pkg/handlers/handlers.go
@@ -27,7 +27,7 @@ func NewHandlers(r *Repository) {
 	Repo = r
 }
 
-// Home is the handler for the home pages
+// Home is the handler for the home page
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
